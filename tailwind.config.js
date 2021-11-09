7472921f47687d9c0aae4071a31abf32dbac8c32
Jules Matsounga <jules.matsounga@bedrockstreaming.com>
2021-11-09 13:38:20 +0100

    fix: safelist color with purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  purge: [],
+  purge: {
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    safelist: [
+      'bg-red-500',
+      'bg-indigo-500',
+      'bg-pink-500',
+      'bg-green-500',
+      'bg-blue-500',
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -8,6 +17,6 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require("daisyui"),
+    require('daisyui'),
   ],
 };
